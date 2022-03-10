34ce2864caf7733d197021bed85b78932c039123
Viachaslau_Barysau <Viachaslau_Barysau@epam.com>
2022-03-10 13:36:16 +0300

    5

--- app/src/main/groovy/groovylab/Task05.groovy
+++ app/src/main/groovy/groovylab/Task05.groovy
@@ -5,8 +5,6 @@ Task05.isSublist("10.0.152.164") == true
 
 package groovylab
 
-package groovylab
-
 class Task05 {
     public static boolean isIp(String ip) {
         if (ip ==~ "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
